a7357a58433aaa5cce7ff8f529f37a7643cc65fc
Marco DE BOOIJ <marco.developer@debooy.eu>
2018-10-14 19:55:19 +0200

    Geen Exception Interceptor meer en lege ArrayList ipv null.

--- src/main/java/eu/debooy/doosutils/access/Dao.java
+++ src/main/java/eu/debooy/doosutils/access/Dao.java
@@ -21,13 +21,15 @@ import eu.debooy.doosutils.domain.DoosSort;
 import eu.debooy.doosutils.domain.Dto;
 import eu.debooy.doosutils.errorhandling.exception.DuplicateObjectException;
 import eu.debooy.doosutils.errorhandling.exception.base.DoosLayer;
-import eu.debooy.doosutils.errorhandling.handler.interceptor.PersistenceExceptionHandlerInterceptor;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
-import javax.interceptor.Interceptors;
 import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
+import javax.persistence.Query;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
@@ -38,14 +40,7 @@ import javax.persistence.criteria.Root;
  * 
  * @author Marco de Booij
  */
-@Interceptors({PersistenceExceptionHandlerInterceptor.class})
 public abstract class Dao<T extends Dto> {
-  /**
-   * Maakt het mogelijk om de Entity Manager in the extended class te definieren
-   * en toch hier te gebruiken.
-   *  
-   * @return De Entity Manager
-   */
   protected abstract  EntityManager getEntityManager();
 
   private Class<T>  dto;
@@ -54,16 +49,10 @@ public abstract class Dao<T extends Dto> {
     this.dto  = dto;
   }
 
-  /**
-   * Schrijf de DTO in de database.
-   * 
-   * @param T Een DTO.
-   * @return T
-   */
   public T create(T dto) {
     if (getEntityManager().contains(dto)) {
-        throw new DuplicateObjectException(DoosLayer.PERSISTENCE, dto,
-                                           "create(T dto)");
+      throw new DuplicateObjectException(DoosLayer.PERSISTENCE, dto,
+                                         "create(T dto)");
     }
 
     getEntityManager().persist(dto);
@@ -73,11 +62,6 @@ public abstract class Dao<T extends Dto> {
     return dto;
   }
 
-  /**
-   * Verwijder de DTO uit de database.
-   * 
-   * @param T Een DTO
-   */
   public void delete(T dto) {
     T merged  = (T) getEntityManager().merge(dto);
     getEntityManager().remove(merged);
@@ -85,86 +69,41 @@ public abstract class Dao<T extends Dto> {
 
   }
 
-  /**
-   * Haal alle rijen op uit de database.
-   * 
-   * @return List<T>
-   */
   public List<T> getAll() {
     CriteriaBuilder   builder   = getEntityManager().getCriteriaBuilder();
     CriteriaQuery<T>  query     = builder.createQuery(dto);
     Root<T>           from      = query.from(dto);
     CriteriaQuery<T>  all       = query.select(from);
-    TypedQuery<T>     allQuery  = getEntityManager().createQuery(all);
 
-    return allQuery.getResultList();
+    return query(all);
   }
 
-  /**
-   * Haal alle rijen op uit de database die aan de filter voldoen.
-   * 
-   * @param Een filter
-   * @return List<T>
-   */
   public List<T> getAll(DoosFilter<T> filter) {
-    CriteriaBuilder   builder   = getEntityManager().getCriteriaBuilder();
-    CriteriaQuery<T>  query     = builder.createQuery(dto);
-    Root<T>           from      = query.from(dto);
-    filter.execute(builder, from, query);
-
-    return getEntityManager().createQuery(query).getResultList();
+    return getAll(filter, null);
   }
 
-  /**
-   * Haal alle rijen op uit de database die aan de filter voldoen gesorteerd op
-   * sort.
-   * 
-   * @param DoosFilter<T> Een filter
-   * @param DoosSort<T> Sorteer parameters
-   * @return List<T>
-   */
   public List<T> getAll(DoosFilter<T> filter, DoosSort<T> sort) {
     CriteriaBuilder   builder   = getEntityManager().getCriteriaBuilder();
     CriteriaQuery<T>  query     = builder.createQuery(dto);
     Root<T>           from      = query.from(dto);
-    filter.execute(builder, from, query);
-    sort.execute(builder, from, query);
+    if (null != filter) {
+      filter.execute(builder, from, query);
+    }
+    if (null != sort) {
+      sort.execute(builder, from, query);
+    }
 
-    return getEntityManager().createQuery(query).getResultList();
+    return query(query);
   }
 
-  /**
-   * Haal alle rijen op gesorteerd op sort.
-   * 
-   * @param DoosSort<T> Sorteer parameters
-   * 
-   * @return List<T>
-   */
   public List<T> getAll(DoosSort<T> sort) {
-    CriteriaBuilder   builder   = getEntityManager().getCriteriaBuilder();
-    CriteriaQuery<T>  query     = builder.createQuery(dto);
-    Root<T>           from      = query.from(dto);
-    sort.execute(builder, from, query);
-
-    return getEntityManager().createQuery(query).getResultList();
+    return getAll(null, sort);
   }
 
-  /**
-   * Krijg de entiteit via de Primary Key
-   *
-   * @param Object primaryKey
-   * @return T
-   */
   public T getByPrimaryKey(Object primaryKey) {
     return getEntityManager().find(dto, primaryKey);
   }
 
-  /**
-   * Geef de dto die behoort bij de gevraagde filter.
-   * 
-   * @param DoosFilter<T> Een filter
-   * @return T
-   */
   public T getUniqueResult(DoosFilter<T> filter) {
     CriteriaBuilder   builder   = getEntityManager().getCriteriaBuilder();
     CriteriaQuery<T>  query     = builder.createQuery(dto);
@@ -176,12 +115,37 @@ public abstract class Dao<T extends Dto> {
     return resultaat;
   }
 
-  /**
-   * Wijzig de data in de database.
-   *  
-   * @param T Een DTO
-   * @return T
-   */
+  public List<T> namedQuery(String querynaam) {
+    return namedQuery(querynaam, new HashMap<String, Object>());
+  }
+
+  @SuppressWarnings("unchecked")
+  public List<T> namedQuery(String querynaam, Map<String, Object> params) {
+    Query query = getEntityManager().createNamedQuery(querynaam);
+
+    for (Entry<String, Object> entry : params.entrySet()) {
+      query.setParameter(entry.getKey(), entry.getValue());
+    }
+
+    List<T> resultaat = query.getResultList();
+
+    if (null == resultaat) {
+      resultaat = new ArrayList<T>();
+    }
+    
+    return resultaat;
+  }
+
+  public List<T> query(CriteriaQuery<T> query) {
+    List<T> resultaat = getEntityManager().createQuery(query).getResultList();
+
+    if (null == resultaat) {
+      resultaat = new ArrayList<T>();
+    }
+
+    return resultaat;
+  }
+
   public T update(T dto) {
     T updated = (T) getEntityManager().merge(dto);
     getEntityManager().persist(updated);
